1aa16bbba9457838e38e21da7db6567f82901d69
Aritra Kumar Dutta <64905199+CarnageX19@users.noreply.github.com>
2024-02-16 23:02:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+
+app = Flask(__name__)
+
+# Database connection
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["vdb"]
+collection = db["vdb-tables"]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    your_name = request.form['yourName']
+    their_name = request.form['theirName']
+    their_number = request.form['theirNumber']
+    message = request.form['message']
+
+    # Insert data into MongoDB
+    document = {"yourName": your_name, "theirName": their_name, "theirNumber": their_number, "message": message}
+    collection.insert_one(document)
+    print("message saved successfully")
+    # Redirect to thank_you.html
+    return redirect(url_for('thank_you'))
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
+
